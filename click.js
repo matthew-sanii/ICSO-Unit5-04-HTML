02d7ea2eb47cefea866893a92088e8f9b448bbd4
matthew.sanii <matthew.sanii@mths.ca>
2020-05-14 13:59:01 +0000

    edited index.html and click.js

--- click.js
+++ click.js
@@ -2,19 +2,18 @@
 let response=document.getElementById("response");
 function Click() {
   let answer=document.getElementById("answer");
-  let answer2=document.getElementById("answer2");
   let answerValue;
   answerValue = Number(answer.value);
-  answer2 = String(answer2);
-  if ((answerValue>12 && answerValue<21)) { // This if statement checks to see if your age is over or equal to 17
-    document.getElementById("myimage").src="./images/check.png";
-    response.innerHTML="You can get the student discount!";
-  }
-  else if (answer2 == "Tuesday" || answer2 == "Thursday") {
-    response.innerHTML="aaaaaaaaaaaaaaaaa";
-  }
+  let answer2=document.getElementById("answer2");
+  answer2 = String(answer2.value);
+  if ((answerValue>12 && answerValue<21) || (answer2 == "Tuesday" || answer2 == "Thursday")) { // This if statement checks to see if your age is between 12 and 21, OR that it is Tuesday or Thursday.
+  document.getElementById("myimage").src="./images/check.png";
+  response.innerHTML="You can get the student discount!"; // This innerHTML line returns that the discount is available.
+}
   else {
-    document.getElementById("myimage").src="./images/X.png";
-    response.innerHTML="You can't get the student discount.";
+  document.getElementById("myimage").src="./images/X.png";
+  response.innerHTML="You can't get the student discount."; // This innerHTML line returns that the discount is unavailable.
   }
 }
+
+ 
